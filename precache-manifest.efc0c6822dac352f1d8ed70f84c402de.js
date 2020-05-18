4fc537e583eb80e4789eee6d87de66a704d53b7d
larakurtovic <lara.kurtovic2001@gmail.com>
2020-05-18 18:45:24 +0200

    Updates

---
+++ precache-manifest.efc0c6822dac352f1d8ed70f84c402de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e1b4d7ca01bb4f7630f8ee30f4eca4",
+    "url": "/larakurtovic/new-react.git/index.html"
+  },
+  {
+    "revision": "45530ad14a07fb783cc3",
+    "url": "/larakurtovic/new-react.git/static/css/main.23b1d036.chunk.css"
+  },
+  {
+    "revision": "4fb4959f9a4751d88269",
+    "url": "/larakurtovic/new-react.git/static/js/2.403ae6fa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/larakurtovic/new-react.git/static/js/2.403ae6fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45530ad14a07fb783cc3",
+    "url": "/larakurtovic/new-react.git/static/js/main.4ab6e7c0.chunk.js"
+  },
+  {
+    "revision": "98e3c3636ffdcdb8302f",
+    "url": "/larakurtovic/new-react.git/static/js/runtime-main.d776bdde.js"
+  },
+  {
+    "revision": "fef43f4e8b985f8c54c2308c151c1858",
+    "url": "/larakurtovic/new-react.git/static/media/palaca.fef43f4e.jpg"
+  },
+  {
+    "revision": "b2f9893b4f70db00ee4c3fa324ffd45c",
+    "url": "/larakurtovic/new-react.git/static/media/palacaa.b2f9893b.jpg"
+  },
+  {
+    "revision": "78f6d1008890c7d6662842e3478b98e1",
+    "url": "/larakurtovic/new-react.git/static/media/split.78f6d100.jpg"
+  },
+  {
+    "revision": "2f7d8311d33155b52257e72ac308f460",
+    "url": "/larakurtovic/new-react.git/static/media/ssplit.2f7d8311.jpg"
+  }
+]);
